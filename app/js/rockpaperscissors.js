3d1c76cd57be61aa8ae248b3b9c8efab6eeda17d
JBA <jba@JBAs-MacBook-Pro.local>
2015-06-09 15:55:33 -0400

    edited rps.js for unit 5 hw

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return PlayerMove = move || getInput();
+    return playerMove = move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return ComputerMove = move || randomPlay();
+    return computerMove = move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -62,8 +62,29 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+	var round = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+	while (playerWins < 5 && computerWins < 5) {
+		getWinner();
+		console.log('Player chose ' + playerMove + 'while Computer chose' + computerMove '.');
+    	if (winner == 'player') {
+			playerWins += 1;
+			console.log('Player wins round ' + round + '.');
+    	} else if (winner == 'computer') {
+    		computerWins +=1;
+			console.log('Computer wins round ' + round + '.';
+    	} else {
+    		console.log('Round ' + round + ' was a tie.');
+   	 	}
+		console.log('The current score is Player: ' + playerWins + 'and Computer: ' + computerWins + '.');
+		round +=1;
+    } else {
+    	if (playerWins == 5) {
+    		console.log("Player wins the game!");
+    	} else {
+    		console.log("Computer wins the game!");
+    	}
+    }
+	return [playerWins, computerWins];
 }
 
